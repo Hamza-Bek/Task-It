10d586c7786d4d7388547678d0590a5f4ced36ef
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2024-08-30 00:19:42 +0300

    Created Mongo's database context

--- Domain/Models/ModelBase.cs
+++ Domain/Models/ModelBase.cs
@@ -1,9 +1,12 @@
 using System;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Domain.Models;
 
 public class ModelBase
 {
+    [BsonId]
+    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
     public string Id { get; set; } = string.Empty;
     public string OwnerId { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
---
+++ Infrastructure/Data/MongoDbContext.cs
@@ -0,0 +1,27 @@
+using System;
+using Amazon.Runtime;
+using MongoDB.Driver;
+
+namespace Infrastructure.Data;
+
+public class MongoDbContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDbContext(string connectionString, string databaseName)
+    {
+        var client = new MongoClient(connectionString);
+        _database = client.GetDatabase(databaseName);
+    }
+
+    /// <summary>
+    /// Retrieves the specified collection from the database
+    /// </summary>
+    /// <typeparam name="T">The data model the collection represents</typeparam>
+    /// <param name="collectionName">The name of the collection</param>
+    /// <returns></returns>
+    public IMongoCollection<T> GetCollection<T>(string collectionName) where T : class
+    {
+        return _database.GetCollection<T>(collectionName);
+    }
+}
