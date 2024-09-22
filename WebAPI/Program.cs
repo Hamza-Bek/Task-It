96275b11fc7cb5478c862ee1b6a04593d4d78b05
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2024-09-22 18:07:14 +0300

    Add database configuration

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,6 @@
 using Application.Features.Todos.Queries;
 using Application.Interfaces;
+using Infrastructure.Data;
 using Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,9 +12,23 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<ITodosRepository, TodosRepository>();
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetTodosHandler).Assembly));
+builder.Services.AddSingleton<MongoDbContext>(sp =>
+{
+    var databaseName = builder.Configuration["Database:Name"];
+    var connectionString = builder.Configuration["Database:ConnectionString"];
 
+    if (string.IsNullOrEmpty(databaseName))
+        throw new NullReferenceException("Database name cannot be null or empty");
+
+    if (string.IsNullOrEmpty(connectionString))
+        throw new NullReferenceException("Database connection string cannot be null or empty");
+
+    var context = new MongoDbContext(connectionString, databaseName);
+    return context;
+});
+
+builder.Services.AddScoped<ITodosRepository, TodosRepository>();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetTodosHandler).Assembly));
 
 var app = builder.Build();
 
